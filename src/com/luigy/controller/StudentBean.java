2409e5a899f93ed9cca336785857b0f0c5a41e37
José Muñoz Sierra <ing.jose.luis@hotmail.com>
2020-06-03 14:39:28 -0500

    add file project - list user implementation

---
+++ src/com/luigy/controller/StudentBean.java
@@ -0,0 +1,32 @@
+package com.luigy.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class StudentBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public List<Student> studentsList() {
+		List<Student> studentList = new ArrayList<Student>(); 
+		
+		Student person1 = new Student(1, "Luis Muñoz", "LuisMunoz@email.com", "12345", "M", "corrientes 6477");
+		Student person2 = new Student(2, "Jose Muñoz", "JoseMunoz@email.com", "12346", "M", "colombia 1234");
+		Student person3 = new Student(3, "Adrian Muñoz", "AdrianMunoz@email.com", "12375", "M", "colombia 234");
+		
+		studentList.add(person1);
+		studentList.add(person2);
+		studentList.add(person3);
+		
+		return studentList;
+	}
+}
